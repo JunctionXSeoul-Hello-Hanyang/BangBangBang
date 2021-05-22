ad8a5f13a093a0d4ea2595084bbf07d0f536c429
강정구 <justin403502@naver.com>
2021-05-22 13:54:47 +0900

    5명받는 테스트

---
+++ connections/Server.py
@@ -0,0 +1,56 @@
+import socket
+from _thread import *
+import sys
+
+server = "127.0.0.1"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+players = {}
+currentPlayer = 0
+
+def threaded_client(conn):
+    conn.send(str.encode("Connected"))
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending : ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    currentPlayer += 1
+
+    if currentPlayer < 5:
+        print(currentPlayer, "players connected")
+        start_new_thread(threaded_client, (conn,))
+    elif currentPlayer == 5:
+        start_new_thread(threaded_client, (conn,))
+        print("Game Starting....")
+
+
