7b28b1f5a862034c89fa47e81223306fe7600ee6
ChoeJaeWon <dnjsdlghdi@gmail.com>
2021-05-22 15:37:20 +0900

    Rule

--- Card/ConsumptionCard/Bang.py
+++ Card/ConsumptionCard/Bang.py
@@ -1,6 +1,10 @@
-from ..ConsumptionCard import ConsumptionCard
+from .. import ConsumptionCard
 
-class EquipmentCard(ConsumptionCard):
+class Bang(ConsumptionCard):
     def __init__(self, image, name, description, number, trump_symbol):
         super().__init__(image, name, description, number, trump_symbol)
+
+    def action(self, target_player_number, turn):
+        turn.bang_turn(target_player_number)
+
 
