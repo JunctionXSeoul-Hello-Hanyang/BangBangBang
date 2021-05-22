7e8c9319628fcf80a0807469272688f9020d5d81
ChoeJaeWon <dnjsdlghdi@gmail.com>
2021-05-22 14:03:04 +0900

    Consumption

--- Card/ConsumptionCard.py
+++ Card/ConsumptionCard.py
@@ -1,6 +1,6 @@
 from Card import PlayingCard
 
-class EquipmentCard(PlayingCard):
+class ConsumptionCard(PlayingCard):
     def __init__(self, image, name, description, number, trump_symbol):
         super().__init__(image, name, description, number, trump_symbol)
 
