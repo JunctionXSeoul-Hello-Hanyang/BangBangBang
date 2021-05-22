e9332ea7b331ca6089273ba7c8d08e1c055dee0f
강정구 <justin403502@naver.com>
2021-05-22 10:28:00 +0900

    UI

---
+++ Card/PlayingCard.py
@@ -0,0 +1,8 @@
+from Card import Card
+
+class PlayingCard(Card):
+    def __init__(self, image, name, number, trump_symbol):
+        super().__init__(image, name)
+        self.number = number
+        self.trump_symbol = trump_symbol
+
