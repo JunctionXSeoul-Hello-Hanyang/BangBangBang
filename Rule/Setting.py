cf3121fa3ad522b76efd1fb3371232c5b581879c
ChoeJaeWon <dnjsdlghdi@gmail.com>
2021-05-22 22:01:00 +0900

    setting

--- Rule/Setting.py
+++ Rule/Setting.py
@@ -1,64 +1,64 @@
 # 플레이어의 수에 따른 직업의 수
 # 보안관, 부관, 무법자, 배신자
 NUMBER_OF_ROLE = {
-    4: ['Sheriff', 'Outlaw', 'Outlaw', 'Renegade'],
-    5: ['Sheriff', 'Deputy', 'Outlaw', 'Outlaw', 'Renegade'],
-    6: ['Sheriff', 'Deputy', 'Outlaw', 'Outlaw', 'Outlaw', 'Renegade'],
-    7: ['Sheriff', 'Deputy', 'Deputy', 'Outlaw', 'Outlaw', 'Outlaw', 'Renegade'],
+    4: ['sheriff', 'dutlaw', 'outlaw', 'renegade'],
+    5: ['sheriff', 'deputy', 'outlaw', 'outlaw', 'renegade'],
+    6: ['sheriff', 'deputy', 'outlaw', 'outlaw', 'outlaw', 'renegade'],
+    7: ['sheriff', 'deputy', 'deputy', 'outlaw', 'outlaw', 'outlaw', 'renegade'],
 }
 
 # PlayingCard: 80
 # H: Heart, D: Diamond, C, Clover, S: Space
 PLAYING_CARD = [
     # ConsumptionCard: 63
-    # Bang!: 25
-    ['Bang!', 'Q', 'H', 'C'], ['Bang!', 'K', 'H', 'C'], ['Bang!', 'A', 'H', 'C'], ['Bang!', 'A', 'D', 'C'],
-    ['Bang!', 'D', 'C', 'C'], ['Bang!', 'D', 'C', 'C'], ['Bang!', '4', 'D', 'C'], ['Bang!', '5', 'D', 'C'],
-    ['Bang!', '6', 'D', 'C'], ['Bang!', '7', 'D', 'C'], ['Bang!', '8', 'D', 'C'], ['Bang!', '9', 'D', 'C'],
-    ['Bang!', '10', 'D', 'C'], ['Bang!', 'J', 'D', 'C'], ['Bang!', 'Q', 'D', 'C'], ['Bang!', 'K', 'D', 'C'],
-    ['Bang!', '2', 'C', 'C'], ['Bang!', '3', 'C', 'C'], ['Bang!', '4', 'C', 'C'], ['Bang!', '5', 'C', 'C'],
-    ['Bang!', '6', 'C', 'C'], ['Bang!', '7', 'C', 'C'], ['Bang!', '8', 'C', 'C'],['Bang!', '9', 'C', 'C'],
-    ['Bang!', 'A', 'S', 'C'],
-    # Missed!: 12
-    ['Missed!', '2', 'S', 'C'], ['Missed!', '3', 'S', 'C'], ['Missed!', '4', 'S', 'C'], ['Missed!', '5', 'S', 'C'],
-    ['Missed!', '6', 'S', 'C'], ['Missed!', '7', 'S', 'C'], ['Missed!', '8', 'S', 'C'], ['Missed!', '10', 'C', 'C'],
-    ['Missed!', 'J', 'C', 'C'], ['Missed!', 'K', 'C', 'C'], ['Missed!', 'A', 'C', 'C'], ['Missed!', 'Q', 'C', 'C'],
-    # Beer: 6
-    ['Beer', '6', 'H', 'C'], ['Beer', '7', 'H', 'C'], ['Beer', '8', 'H', 'C'], ['Beer', '9', 'H', 'C'],
-    ['Beer', '10', 'H', 'C'], ['Beer', 'J', 'H', 'C'],
-    # Duel: 3
-    ['Duel', 'Q', 'D', 'C'], ['Duel', '8', 'C', 'C'], ['Duel', 'J', 'H', 'C'],
-    # Indian!: 2
-    ['Indian!', 'K', 'D', 'C'], ['Indian!', 'A', 'D', 'C'],
+    # bang: 25
+    ['bang', 'Q', 'H', 'C'], ['bang', 'K', 'H', 'C'], ['bang', 'A', 'H', 'C'], ['bang', 'A', 'D', 'C'],
+    ['bang', 'D', 'C', 'C'], ['bang', 'D', 'C', 'C'], ['bang', '4', 'D', 'C'], ['bang', '5', 'D', 'C'],
+    ['bang', '6', 'D', 'C'], ['bang', '7', 'D', 'C'], ['bang', '8', 'D', 'C'], ['bang', '9', 'D', 'C'],
+    ['bang', '10', 'D', 'C'], ['bang', 'J', 'D', 'C'], ['bang', 'Q', 'D', 'C'], ['bang', 'K', 'D', 'C'],
+    ['bang', '2', 'C', 'C'], ['bang', '3', 'C', 'C'], ['bang', '4', 'C', 'C'], ['bang', '5', 'C', 'C'],
+    ['bang', '6', 'C', 'C'], ['bang', '7', 'C', 'C'], ['bang', '8', 'C', 'C'],['bang', '9', 'C', 'C'],
+    ['bang', 'A', 'S', 'C'],
+    # missed: 12
+    ['missed', '2', 'S', 'C'], ['missed', '3', 'S', 'C'], ['missed', '4', 'S', 'C'], ['missed', '5', 'S', 'C'],
+    ['missed', '6', 'S', 'C'], ['missed', '7', 'S', 'C'], ['missed', '8', 'S', 'C'], ['missed', '10', 'C', 'C'],
+    ['missed', 'J', 'C', 'C'], ['missed', 'K', 'C', 'C'], ['missed', 'A', 'C', 'C'], ['missed', 'Q', 'C', 'C'],
+    # beer: 6
+    ['beer', '6', 'H', 'C'], ['beer', '7', 'H', 'C'], ['beer', '8', 'H', 'C'], ['beer', '9', 'H', 'C'],
+    ['beer', '10', 'H', 'C'], ['beer', 'J', 'H', 'C'],
+    # duel: 3
+    ['duel', 'Q', 'D', 'C'], ['duel', '8', 'C', 'C'], ['duel', 'J', 'H', 'C'],
+    # indian!: 2
+    ['indian!', 'K', 'D', 'C'], ['indian!', 'A', 'D', 'C'],
     # Gatling: 1
-    ['Gatling', '10', 'H', 'C'],
+    ['gatling', '10', 'H', 'C'],
     # Saloon: 1
-    ['Saloon', '5', 'H', 'C'],
-    # Panic!: 4
-    ['Panic!', '8', 'D', 'C'], ['Panic!', 'J', 'H', 'C'], ['Panic!', 'Q', 'H', 'C'], ['Panic!', 'A', 'H', 'C'],
-    # CatBalu: 4
-    ['CatBalu', '9', 'D', 'C'], ['CatBalu', '10', 'D', 'C'], ['CatBalu', 'J', 'D', 'C'], ['CatBalu', 'K', 'H', 'C'],
+    ['saloon', '5', 'H', 'C'],
+    # panic: 4
+    ['panic', '8', 'D', 'C'], ['panic', 'J', 'H', 'C'], ['panic', 'Q', 'H', 'C'], ['panic', 'A', 'H', 'C'],
+    # catBalu: 4
+    ['catBalu', '9', 'D', 'C'], ['catBalu', '10', 'D', 'C'], ['catBalu', 'J', 'D', 'C'], ['catBalu', 'K', 'H', 'C'],
     # GeneralStore: 2
-    ['GeneralStore', '9', 'C', 'C'], ['GeneralStore', 'Q', 'H', 'C'],
+    ['generalStore', '9', 'C', 'C'], ['generalStore', 'Q', 'H', 'C'],
     # Stagecoach: 2
-    ['Stagecoach', '9', 'S', 'C'], ['Stagecoach', '9', 'S', 'C'],
+    ['stagecoach', '9', 'S', 'C'], ['stagecoach', '9', 'S', 'C'],
     # WellsFargo: 1
-    ['WellsFargo', '3', 'H', 'C'],
+    ['wellsFargo', '3', 'H', 'C'],
 
     # ConsumptionCard: 17
     # Gun: 8
-    ['Schofield', 'J', 'C', 'EG'], ['Schofield', 'Q', 'C', 'EG'], ['Schofield', 'K', 'S', 'EG'], ['Remington', 'A', 'C', 'EG'],
-    ['Remington', '5', 'S', 'EG'], ['Winchester', '8', 'S', 'EG'], ['Volcanic', '10', 'C', 'EG'], ['Volcanic', '10', 'S', 'EG'],
+    ['schofield', 'J', 'C', 'EG'], ['schofield', 'Q', 'C', 'EG'], ['schofield', 'K', 'S', 'EG'], ['remington', 'A', 'C', 'EG'],
+    ['remington', '5', 'S', 'EG'], ['winchester', '8', 'S', 'EG'], ['volcanic', '10', 'C', 'EG'], ['volcanic', '10', 'S', 'EG'],
     # Jail: 3
-    ['Jail', '4', 'H', 'E'], ['Jail', '10', 'S', 'E'], ['Jail', 'J', 'S', 'E'],
+    ['jail', '4', 'H', 'E'], ['jail', '10', 'S', 'E'], ['jail', 'J', 'S', 'E'],
     # Barrel: 2
-    ['Barrel', 'Q', 'S', 'E'], ['Barrel', 'K', 'S', 'E'],
+    ['barrel', 'Q', 'S', 'E'], ['barrel', 'K', 'S', 'E'],
     # Mustang: 2
-    ['Mustang', '8', 'H', 'E'], ['Mustang', '9', 'H', 'E'],
+    ['mustang', '8', 'H', 'E'], ['mustang', '9', 'H', 'E'],
     # Scope: 1
-    ['WellsFargo', 'A', 'S', 'E'],
+    ['wellsFargo', 'A', 'S', 'E'],
     # Dynamite: 1
-    ['Dynamite', '2', 'H', 'E'],
+    ['dynamite', '2', 'H', 'E'],
 ]
 
 IMAGE_ROLE_PATH = '../UI/ImageAsset/roles/'
