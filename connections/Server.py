7b9e79274963db50a5e71a32e87b2f148a46a72e
황태용 <xodyd15@naver.com>
2021-05-22 21:34:18 +0900

    서버 수정1

--- connections/Server.py
+++ connections/Server.py
@@ -1,11 +1,13 @@
 import socket
 from _thread import *
+
 import sys
 
 server = "0.0.0.0"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 try:
     s.bind((server, port))
@@ -15,13 +17,19 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started")
 
-players = {}
+playersSocket = []
 currentPlayer = 0
+board = None
 
 def threaded_client(conn):
     conn.send(str.encode("Connected"))
+    
+    # 맨 처음 게임판을 전달하는 작업
+    conn.sendall('전달해야되는 값')
+
     reply = ""
     while True:
+        # 여기선 서버의 경우 정보를 받는 경우에만 동작하게 된다. (턴제로)
         try:
             data = conn.recv(2048)
             reply = data.decode("utf-8")
@@ -40,17 +48,24 @@ def threaded_client(conn):
     print("Lost connection")
     conn.close()
 
+def broadcast_board(board):
+
+
+
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
 
+    print(currentPlayer, "players connected")
     currentPlayer += 1
+    playersSocket.append(conn);
 
-    if currentPlayer < 5:
-        print(currentPlayer, "players connected")
-        start_new_thread(threaded_client, (conn,))
-    elif currentPlayer == 5:
-        start_new_thread(threaded_client, (conn,))
+    if currentPlayer == 5:
         print("Game Starting....")
+        board = Board(5) # 게임판 생성
+        for conn in playersSocket:
+            start_new_thread(threaded_client, (conn, ))
+
+
 
 
