d4f29792d32a4d46747bf115168988cb9662dbab
황태용 <xodyd15@naver.com>
2021-05-23 02:28:53 +0900

    소켓부분 핸들링

--- connections/Network.py
+++ connections/Network.py
@@ -1,34 +1,35 @@
 import socket
-
+import pickle
 
 class Network:
     def __init__(self, serverIp, serverPort):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print(self.client)
         self.server = serverIp
         self.port = serverPort
         self.addr = (self.server, self.port)
+        self.board = None
         self.id = self.connect()
+
         print(self.id)
 
     def connect(self):
         try:
             self.client.connect(self.addr)
+            self.board = pickle.loads(self.client.recv(4096))
             return self.client.recv(2048).decode()
         except Exception as e:
             print(e)
-            pass
 
     def close(self):
         try:
             self.client.close()
-        except:
-            pass
+        except Exception as e:
+            print(e)
 
 
     def send(self, data):
         try:
             self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(4096))
         except socket.error as e:
             print(e)
