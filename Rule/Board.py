e0e757e2770af6858369c16b1b7241b179b1c14d
ChoeJaeWon <dnjsdlghdi@gmail.com>
2021-05-22 15:26:32 +0900

    Rule

--- Rule/Board.py
+++ Rule/Board.py
@@ -11,14 +11,6 @@ class Board:
         self.deck = None
         self.trashCan = None
 
-    def gameStart(self):
-        role_temp = random.shuffle(Setting.NUMBER_OF_ROLE[self.number_of_players])
-        self.players = [Player() for i in range(self.number_of_players)]
-        self.deck = initDeck()
-
-
-    def initDeck(self):
-
 
 
 
---
+++ Rule/Turn.py
@@ -0,0 +1,3 @@
+class Turn:
+    def bang_turn(self, target_player):
+        return 0
