cc9a3d5deac4a711d8e55295aed0e0a2b3236223
김태욱 <qkenr7895@hanyang.ac.kr>
2021-05-22 13:39:39 +0900

    test
    a

--- UI/test.py
+++ UI/test.py
@@ -2,5 +2,8 @@ from BoardUI import BoardUI
 import time
 
 if __name__ == "__main__":
+    # 기본 보드 생성
     b = BoardUI()
+    # 특정 위치에 카드 그리기
+    # b.drawCard("Sheriff", "player1")
     time.sleep(100)
