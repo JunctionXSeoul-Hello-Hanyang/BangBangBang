51f523153082b45c21a8fc4c5c67e40ab0e1a94f
ChoeJaeWon <dnjsdlghdi@gmail.com>
2021-05-22 18:11:53 +0900

    Card, Field

--- Rule/Field.py
+++ Rule/Field.py
@@ -1,3 +1,7 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from Card import Card
+
 class Field:
     def __init__(self, bullets, role, character):
         self.image = None
@@ -5,7 +9,7 @@ class Field:
         self.bullets = bullets
         self.role = role
         self.character = character
-        self.gunCard = 'Colt45'
+        self.gunCard = Card('Colt.45', 'you can only hit targets at a distance of 1', 'EG', None, None)
         self.equipmentCards = None
 
 
